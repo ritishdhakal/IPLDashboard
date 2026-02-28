4b8dff28a62e3058ee5fa0ceb952036f04842c62
ritishdhakal <dhakalritish@gmail.com>
2026-03-01 00:02:21 +1100

    Calculate total matches and wins for each team after batch job
    Update Team entity with totalMatches and totalWins after job completion
    
    Implement afterJob listener to aggregate team statistics
    
    Add logic to merge team stats (matches & wins) into database

--- src/main/java/io/ritish/ipl_dashboard/data/JobCompletionNotificationListener.java
+++ src/main/java/io/ritish/ipl_dashboard/data/JobCompletionNotificationListener.java
@@ -6,30 +6,76 @@ import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.DataClassRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import io.ritish.ipl_dashboard.model.Match;
+import io.ritish.ipl_dashboard.model.Team;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 
 @Component
 public class JobCompletionNotificationListener implements JobExecutionListener {
 
     private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
-    private final JdbcTemplate jdbcTemplate;
+    private final EntityManager em;
 
-    public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public JobCompletionNotificationListener(EntityManager em) {
+        this.em = em;
     }
 
     @Override
-    public void afterJob(JobExecution jobExecution) {
+    @Transactional
+    public void afterJob(JobExecution jobExecution) { // it runs after the job is completed, and it will be used to
+                                                      // calculate the total matches and total wins for each team and
+                                                      // save it to the database
         if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
             log.info("!!! JOB FINISHED! Time to verify the results");
 
-            jdbcTemplate
-                    .query("SELECT id,team1, team2, umpire1, umpire2 FROM match",
-                            new DataClassRowMapper<Match>(Match.class))
-                    .forEach(match -> log.info("Found <{}> in the database.", match));
+            // since we have two teams in each match, we need to calculate the total matches
+            // for each team by
+            // counting the number of times each team appears as team1 and team2 in the
+            // Match table,
+            // and then we need to calculate the total wins for each team by counting
+            // the number of times each team appears as matchWinner in the Match table, and
+            // then we need to save the results to the Team table
+
+            Map<String, Team> teamData = new HashMap<>();
+
+            em.createQuery("select m.team1, count(*) from Match m group by m.team1", Object[].class)
+                    .getResultList()
+                    .stream()
+                    .map(e -> new Team((String) e[0], (long) e[1]))// converting "MI" , 100 into Team ("MI", 100)
+
+                    .forEach(team -> teamData.put(team.getTeamName(), team));
+
+            em.createQuery("select  m.team2, count(*) as matches from Match m group by m.team2", Object[].class)
+                    .getResultList()
+                    .stream()
+                    .forEach(e -> {
+                        Team team = teamData.get((String) e[0]);
+                        team.setTotalMatches(team.getTotalMatches() + (long) e[1]);
+                    });
+
+            em.createQuery("select m.matchWinner, count(*) from Match m group by m.matchWinner", Object[].class)
+                    .getResultList()
+                    .stream()
+                    .forEach(e -> {
+                        Team team = teamData.get((String) e[0]);
+                        if (team != null)
+                            team.setTotalWins((long) e[1]);
+                    });
+
+            // saving result to the database
+            teamData.values().forEach(team -> em.merge(team));
+
+            teamData.values().forEach(team -> System.out.println(team));
+
         }
     }
 }
---
+++ src/main/java/io/ritish/ipl_dashboard/model/Team.java
@@ -0,0 +1,63 @@
+package io.ritish.ipl_dashboard.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String teamName;
+    private long totalMatches;
+    private long totalWins;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public void setTotalMatches(long totalMatches) {
+        this.totalMatches = totalMatches;
+    }
+
+    public void setTotalWins(long totalWins) {
+        this.totalWins = totalWins;
+    }
+
+    public long getTotalMatches() {
+        return totalMatches;
+    }
+
+    public long getTotalWins() {
+        return totalWins;
+    }
+
+    public Team() {
+    }
+
+    public Team(String teamName, long totalMatches) {
+        this.teamName = teamName;
+        this.totalMatches = totalMatches;
+    }
+
+    @Override
+    public String toString() {
+        return "Team [id=" + id + ", teamName=" + teamName + ", totalMatches=" + totalMatches + ", totalWins="
+                + totalWins + "]";
+    }
+
+}
